e01efd9d9526182ea2fc4f80b379371cb0c2d509
Rahul <111289008+rahulsingh2312@users.noreply.github.com>
2023-04-24 02:45:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -50,15 +50,18 @@ function animateTitle() {
 if(i==1){
  vnavjs.style.opacity=0;
  scrolldownlink.style.pointerEvents="all";
- scrolldownlink.style.opacity=1;
+ scrolldownlink.style.opacity=1;   
+ let inputs = document.getElementById("check");
+ inputs.checked=false;
  i=0;
 }
 else{
   bodyrjs.style.backgroundColor="   #eeaeca";
-  setTimeout(myFunctionwhite, 7000);
+  setTimeout(myFunctionwhite, 17000);
   scrolldownlink.style.pointerEvents="none";
   vnavjs.style.opacity=1;
    scrolldownlink.style.opacity=0;
+
   i=1;
 }
  
@@ -84,14 +87,54 @@ else{
         myNav.style.backgroundColor="transparent";
 
     } 
-    if (window.scrollY>=200 ) {
-
-     
-
-  } 
+    var tbl1 = document.getElementById("one");
+    var tbl2 = document.getElementById("two");
+    var tbl3 = document.getElementById("three");
+    function colrchng1(){
+  
+  tbl1.style.backgroundColor="#c5f8c8";
+  
+    }
+    function colrchng2(){
+  
+  
+  tbl2.style.backgroundColor="#fccb8f";
+  
+    }
+    function colrchng3(){
+  
+  
+  tbl3.style.backgroundColor="#f9989f";
+    }
+if(   window.scrollY>650){
+  colrchng1();
+   colrchng2();
+   colrchng3();
+}
+if(window.scrollY<649){
+  tbl3.style.backgroundColor="white";
+  tbl2.style.backgroundColor="white";
+  tbl1.style.backgroundColor="white";
+}
+if(window.scrollY>1300){
+  bodyrjs.style.backgroundColor="black";
+ var workte = document.getElementById("work");
+ workte.style.color="white";
+ var workteone = document.getElementById("takeitleft");
+ workteone.style.color="white";
+ var workteone1 = document.getElementById("describework1");
+ workteone1.style.color="white";
+}
     
       
   };
+  function touchcolor(){
+    var y = event.touches[0].clientY;
+    if(y>1100){
+    tbl1.style.backgroundColor="#c5f8c8";
+    tbl3.style.backgroundColor="#f9989f";
+    tbl2.style.backgroundColor="#fccb8f";}
+  }
   var tbl1 = document.getElementById("one");
   var tbl2 = document.getElementById("two");
   var tbl3 = document.getElementById("three");
