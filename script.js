bce627f697502bf0938a2201b9b7f0b4e01fa2da
Rahul <111289008+rahulsingh2312@users.noreply.github.com>
2023-05-03 02:19:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -38,10 +38,12 @@ function animateTitle() {
       }
       counts += 1 / 6;
     }, 30);
+    
   }
   
   window.onload = animateTitle();
   var i=0;
+
   function hamfunc(){
     var bodyrjs = document.getElementById("bodyhtml")
  var scrolldownlink = document.getElementById("scrollDown")
@@ -50,7 +52,8 @@ function animateTitle() {
 if(i==1){
  vnavjs.style.opacity=0;
  scrolldownlink.style.pointerEvents="all";
- scrolldownlink.style.opacity=1;   
+ scrolldownlink.style.opacity=1;  
+ vnavjs.style.pointerEvents="none"; 
  let inputs = document.getElementById("check");
  inputs.checked=false;
  i=0;
@@ -60,6 +63,8 @@ else{
   setTimeout(myFunctionwhite, 17000);
   scrolldownlink.style.pointerEvents="none";
   vnavjs.style.opacity=1;
+  vnavjs.style.pointerEvents="all";
+  vnavjs.style.cursor="crosshair"
    scrolldownlink.style.opacity=0;
 
   i=1;
@@ -76,7 +81,13 @@ else{
       if (window.scrollY>=200 ) {
         console.log(scrollY);
         bodyrjs.style.backgroundColor="white";
-          myNav.style.backgroundColor="violet";
+        var workte = document.getElementById("work");
+        workte.style.color="black";
+        var workteone = document.getElementById("takeitleft");
+        workteone.style.color="black";
+        var workteone1 = document.getElementById("describework1");
+        workteone1.style.color="black";
+          myNav.style.backgroundColor="black";
       } 
           
       else if(window.scrollY<=200&&window.scrollY>=100) {
@@ -125,16 +136,84 @@ if(window.scrollY>1300){
  var workteone1 = document.getElementById("describework1");
  workteone1.style.color="white";
 }
-    
+if(window.scrollY>2200){
+  bodyrjs.style.backgroundColor="#FFDCB6";
+ var workte = document.getElementById("work");
+ workte.style.color="white";
+ var workteone = document.getElementById("takeitleft");
+ workteone.style.color="white";
+ var workteone1 = document.getElementById("describework1");
+ workteone1.style.color="white";
+ var workteone2 = document.getElementById("describework2");
+ workteone2.style.color="black";
+workteone2.style.backgroundColor="#FFDCB6";
+}
+if(window.scrollY>2600){
+  bodyrjs.style.backgroundColor="#9384D1";
+ var titlework3j = document.getElementById("titlework3");
+ titlework3j.style.color="white";
+ myNav.style.backgroundColor="#E3F2C1";
+//  var workteone = document.getElementById("takeitleft");
+//  workteone.style.color="white";
+ var workteone2 = document.getElementById("describework2");
+ workteone2.style.color="white";
+workteone2.style.backgroundColor="#9384D1";
+
+}
+if(window.scrollY>3100){
+  bodyrjs.style.backgroundColor="#EFBBCF";
+ var titlework3j = document.getElementById("titlework3");
+ titlework3j.style.color="white";
+ myNav.style.backgroundColor="#C0DBEA";
+}  
       
   };
   function touchcolor(){
     var y = event.touches[0].clientY;
+    console.log(y);
     if(y>1100){
     tbl1.style.backgroundColor="#c5f8c8";
     tbl3.style.backgroundColor="#f9989f";
     tbl2.style.backgroundColor="#fccb8f";}
-  }
+    if(y<1010 ){
+      tbl1.style.backgroundColor="white";
+      tbl3.style.backgroundColor="white";
+      tbl2.style.backgroundColor="white";
+    }
+    // 
+    if(y<1799){
+      bodyrjs.style.backgroundColor="white";
+      var workte = document.getElementById("work");
+      workte.style.color="black";
+      var workteone = document.getElementById("takeitleftmob");
+      workteone.style.color="black";
+      var workteone1 = document.getElementById("describework1");
+      workteone1.style.color="black";
+      bodyrjs.style.color="black";
+    }
+    if(y>1800){
+      bodyrjs.style.backgroundColor="black";
+      var workte = document.getElementById("work");
+      workte.style.color="white";
+      var workteone = document.getElementById("takeitleftmob");
+      workteone.style.color="white";
+      var workteone1 = document.getElementById("describework1");
+      workteone1.style.color="white";
+      var tit = document.getElementById("titleee");
+      tit.style.color="red";
+
+
+    }
+    if(y>2100){
+      bodyrjs.style.backgroundColor="#ECC9EE";
+      
+     
+    }
+    if(y>2350){
+      bodyrjs.style.backgroundColor="#EFBBCF";
+    }
+    
+     }
   var tbl1 = document.getElementById("one");
   var tbl2 = document.getElementById("two");
   var tbl3 = document.getElementById("three");
